042c91fc5b4457d2f49d3f11aa1a80dc71fce7f0
developerwill <info@wilsongomes.me>
2022-12-30 21:26:26 +0100

    Linear gradient + image

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import {StyleSheet} from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
 import StartGame from "./screens/StartGame";
 import {LinearGradient} from "expo-linear-gradient";
 
 export default function App() {
-  return(
-      <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-        <StartGame/>
-      </LinearGradient>
-  );
+    return (
+        <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+            <ImageBackground
+                style={styles.rootScreen}
+                source={require('./assets/background.png')}
+                resizeMode={'cover'}
+                imageStyle={styles.imageBackground}
+            >
+                <StartGame/>
+            </ImageBackground>
+        </LinearGradient>
+    );
 }
 
 const styles = StyleSheet.create({
-  rootScreen: {
-    flex: 1
-  }
+    rootScreen: {
+        flex: 1
+    },
+    imageBackground: {
+        opacity: 0.15
+    }
 });
