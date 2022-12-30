ed4b82a68714a1391b072d75e5043731868ba1ab
developerwill <info@wilsongomes.me>
2022-12-30 22:22:56 +0100

    Managing Colors Globally

--- screens/StartGame.jsx
+++ screens/StartGame.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import {Alert, StyleSheet, TextInput, View} from "react-native";
 import PrimaryBtn from "../components/buttons/PrimaryBtn";
+import Colors from "../constants/colors";
 
 export default function StartGame({onPickNumber}) {
     const [enteredNumber, setEnteredNumber] = useState('');
@@ -57,7 +58,7 @@ const styles = StyleSheet.create({
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
-        backgroundColor: '#3b021f',
+        backgroundColor: Colors.primary800,
         borderRadius: 8,
         elevation: 4
     },
@@ -65,9 +66,9 @@ const styles = StyleSheet.create({
         height: 50,
         width: 50,
         fontSize: 32,
-        borderBottomColor: '#ddb52f',
+        borderBottomColor: Colors.accent500,
         borderBottomWidth: 2,
-        color: '#ddb52f',
+        color: Colors.accent500,
         marginVertical: 8,
         fontWeight: 'bold',
         textAlign: 'center'
