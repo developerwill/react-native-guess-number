e5e8f852091c89ff0eace89589ba540a9d60668d
developerwill <info@wilsongomes.me>
2022-12-30 20:37:48 +0100

    Styling

---
+++ components/buttons/PrimaryBtn.jsx
@@ -0,0 +1,11 @@
+import {Text, View} from "react-native";
+
+function PrimaryBtn({children}) {
+    return (
+      <View>
+          <Text>{children}</Text>
+      </View>
+    );
+}
+
+export default PrimaryBtn;
--- screens/StartGame.jsx
+++ screens/StartGame.jsx
@@ -1,9 +1,34 @@
-import {TextInput, View} from "react-native";
+import {StyleSheet, TextInput, View} from "react-native";
+import PrimaryBtn from "../components/buttons/PrimaryBtn";
 
 export default function StartGame() {
     return (
-        <View>
-            <TextInput></TextInput>
+        <View style={styles.inputContainer}>
+            <TextInput style={styles.numberInput} maxLength={2}/>
+            <PrimaryBtn>Reset</PrimaryBtn>
+            <PrimaryBtn>Confirm</PrimaryBtn>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#72063c',
+        borderRadius: 8,
+        elevation: 4
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+});
